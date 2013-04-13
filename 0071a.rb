516072ba73094ea66ba8abcc2fed8de0fad23ad6
Philippe Rigovanov <phil@rigovanov.ru>
2013-04-13 16:29:31 +0400

    solved third problem

--- 0071a.rb
+++ 0071a.rb
@@ -1,19 +1,4 @@
-# http://codeforces.ru/problemset/problem/158/A
-n,k = gets.split.map { |e| e.to_i}
-a = gets.split
-# print "ak = "
-ak = a[k-1].to_i # puts ak = a[k-1].to_i
-# print "x = "
-x = k # puts x = k
-if ak > 0 then
-    (k...n).each do |i|
-		x += 1 if a[i].to_i == ak 
-	end
-else
-	x = 0
-	(0...(k-1)).each do |i|
-		x += 1 if a[i].to_i > 0
-	end
+# http://codeforces.ru/problemset/problem/71/A
+gets.to_i.times do |i|
+	puts (len = (word = gets.chomp).length) <= 10 ? word : word[0] + (len - 2).to_s + word[len - 1]
 end
-# print "Ответ: "
-puts x
